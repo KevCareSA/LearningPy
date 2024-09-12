942aaa2f5fedbbee3c357de95322da0d7a2d36d8
KevCareSA <kev.mkbn2@gmail.com>
2024-09-12 23:34:32 +0200

    feat: practice Python expressions including variable assignments, arithmetic operations, and type conversions

---
+++ Basic_Practice/expressions.py
@@ -0,0 +1,17 @@
+# a = int(input("input a here: "))
+# b = int(input("input b here: "))
+# h = int(input("input b here: "))
+
+# area = 1/2 * (a+b) * h
+
+# print("the area of a rombus is:",area)
+
+
+#Area of a triangle
+
+base = int(input("input base here: "))
+height = int(input("input height here: "))
+
+triangle = 1/2 * base * height
+
+print("Area of triangle:", triangle)
