079914f124e6116497d49706bc4c8efc66e87ad2
KevCareSA <kev.mkbn2@gmail.com>
2024-09-13 18:09:01 +0200

    feat: practice Python expressions including variable assignments

--- Basic_Practice/expressions.py
+++ Basic_Practice/expressions.py
@@ -9,9 +9,51 @@
 
 #Area of a triangle
 
-base = int(input("input base here: "))
-height = int(input("input height here: "))
+# base = int(input("input base here: "))
+# height = int(input("input height here: "))
 
-triangle = 1/2 * base * height
+# triangle = 1/2 * base * height
 
-print("Area of triangle:", triangle)
+# print("Area of triangle:", triangle)
+
+
+# #Area of Circle
+
+# Pi = 3.14
+# r = float(input('Enter radius '))
+
+# Area = Pi * r**2
+
+
+# print('The area of a circle is: ', Area)
+
+
+
+#Is Odd or is Even
+# Num = int(input("Enter Number "))
+
+# if Num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+#Vote Cast
+
+# age = int(input("Enter age "))
+
+# if age < 18:
+#     print("Not Eligibale to vote")
+# else:
+#     print("Eligible to vote")
+
+#Check Marks
+
+mark = int(input("Enter marks "))
+low_mark = 0
+high_mark = 100
+
+if low_mark <= mark <= high_mark:
+    print("Within range")
+else:
+    print("Outside range") 
