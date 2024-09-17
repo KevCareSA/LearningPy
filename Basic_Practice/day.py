2d0df3afdab55e4ee9187525453ca55a756f9ca0
KevCareSA <kev.mkbn2@gmail.com>
2024-09-17 23:50:36 +0200

    Docs: python Notes

---
+++ Basic_Practice/day.py
@@ -0,0 +1,20 @@
+#Day number and display day name
+
+day_Num = int(input("Enter number of day :"))
+
+if day_Num == 1:
+    print("Monday")
+elif day_Num == 2: # type: ignore
+    print("Tuesday")
+elif day_Num == 3:
+    print("Wednesday")
+elif day_Num == 4:
+    print("Thursday")
+elif day_Num == 5:
+    print("Friday")
+elif day_Num == 6:
+    print("Saturday")
+else: 
+    day_Num == 7
+    print("Sunday")
+    
